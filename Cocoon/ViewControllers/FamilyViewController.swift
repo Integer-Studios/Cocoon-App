6db546c2dd119686dcf008f40f87e17415e8edc4
Jake Trefethen <hardcorebadger@Jakes-MacBook-Pro.local>
2015-06-24 13:14:39 -0700

    menu, family table view

--- Cocoon/ViewControllers/FamilyViewController.swift
+++ Cocoon/ViewControllers/FamilyViewController.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
-class FamilyViewController: UIViewController {
+class FamilyViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var items = ["Fuck", "Kids"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        //load dem kids
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("selected: \(items[indexPath.row])")
     }
     
     @IBAction func close(sender: AnyObject) {
