e224774cfae34bd3ad25f39757eeccd9d5e8d2f0
Quinn Finney <quinn.finney@gmail.com>
2015-10-21 16:58:48 -0700

    Request Ride Map

--- Cocoon/ViewControllers/RequestViewController.swift
+++ Cocoon/ViewControllers/RequestViewController.swift
@@ -7,20 +7,98 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class RequestViewController: UIViewController {
+class RequestViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UITabBarDelegate {
+    
+    @IBOutlet weak var tabBar: UITabBar!;
+    @IBOutlet weak var mapView: MKMapView!
+
+    var locationManager: CLLocationManager?
+    var pointAnnotation: MKPointAnnotation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (locationManager == nil) {
+            
+            locationManager = CLLocationManager()
+            
+        }
+        tabBar.delegate = self;
 
+        self.locationManager?.delegate = self;
+        self.locationManager?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        self.locationManager?.requestWhenInUseAuthorization()
+        
+        self.locationManager?.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        self.locationManager?.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Errors: " + error.localizedDescription)
+    }
+    
+    func setMapAddress() {
+        
+        let coord = CLLocationCoordinate2D(latitude: 34.427883, longitude: -119.874690)
+        self.mapView.showsUserLocation = false
+        self.mapView.setCenterCoordinate(coord, animated: true)
+        
+        if (pointAnnotation != nil) {
+            self.mapView.removeAnnotation(self.pointAnnotation!)
+            self.pointAnnotation = nil
+        }
+        self.pointAnnotation = MKPointAnnotation()
+        self.pointAnnotation?.title = "Address"
+        self.pointAnnotation?.coordinate = coord
+        self.mapView.addAnnotation(self.pointAnnotation!)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        
+
+        if (item.tag == 1) {
+            
+            if (self.pointAnnotation != nil) {
+                
+                self.mapView.removeAnnotation(self.pointAnnotation!)
+                self.pointAnnotation = nil
+                
+            }
+            self.mapView.showsUserLocation = true
+            self.locationManager?.startUpdatingLocation()
+            
+        } else {
+        
+            setMapAddress()
+            
+        }
+        
+        
+    }
 
     /*
     // MARK: - Navigation
